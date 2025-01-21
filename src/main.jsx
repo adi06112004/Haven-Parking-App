4cf14ceaaacbb87f77cc3dfe4d9cb7a3e48fd152
Aditya Prasad <adityak2541@gmail.com>
2025-01-21 18:33:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Haven-Parking-App">
     <App />
     </BrowserRouter>
   </StrictMode>,
